b88043a4c334be62f90d13365e0aa7ba546cff2f
Ben Li <benli3060@gmail.com>
2020-03-20 11:55:49 -0700

    fix linking issue

--- src/message.c
+++ src/message.c
@@ -4,10 +4,14 @@
 
 //message type
 #include <stdio.h>
+#include <avro.h>
+#include <avro/io.h>
 #include "common.h"
 #include "message.h"
 #include "log.h"
 
+
+#define SCHEMA_PATH "../schema/message.json"
 int prepare_message(char *header, char *data, message_t *message) {
     sprintf(message->header, "%s", header);
     sprintf(message->data, "%s", data);
@@ -17,6 +21,28 @@ int prepare_message(char *header, char *data, message_t *message) {
 int print_message(message_t *message) {
     log_info("Message: \"%s: %s\"", message->header, message->data);
     return 0;
+}
+
+
+int readSchema(char * fileName, char* content) {
+    FILE * file = fopen(fileName, "r");
+    if (file == NULL) {
+        return -1;
+    }
+    int size = fread(file, sizeof(&content),1, content);
+    fclose(file);
+    if (size <= 0) {
+        return -1;
+    } else {
+        return size;
+    }
+}
+
+int validateSchema() {
+    char schema[8192];
+    FILE * file = fopen(SCHEMA_PATH, "r");
+    avro_reader_file(file);
+    avro_reader_t * reader = avro_reader_file(file);
 }
 
 // message queue --------------------------------------------------------------
